c1b1eaf835063756f72aedc78dbd0799f6ca6ee4
JimS20 <jimstam@hotmail.com>
2017-06-22 10:36:57 +0200

    Manager,Iterator + UML update

---
+++ Project4/Project4/Iterator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Project4
+{
+    public interface IIterator<T>
+    {
+        IOption<T> GetNext();
+        void Reset();
+    }
+
+    public class EntityIterator<Entity> : IIterator<Entity>
+    {
+        List<Entity> Entities = new List<Entity>();
+
+        public IOption<Entity> GetNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project4/Project4/Managers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Project4
+{
+    public class EntityManager : EntityIterator<Entity>, IComponent
+    {
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(float dt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
