bc4e53138a1182ad16096001815fa21038a45533
JimS20 <jimstam@hotmail.com>
2017-06-30 14:50:06 +0200

    ????I DONT KNOW ANYMORE

---
+++ NonSFML/Project/Project4/DataBase.cs
@@ -0,0 +1,53 @@
+﻿//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Graphics;
+//using Microsoft.Xna.Framework.Input;
+
+//using System.Data.SQLite;
+//using System.Data;
+
+//namespace Project4
+//{
+//    public class Database
+//    {
+//        public string sql;
+//        public void Data()
+//        {
+//            //SQLiteConnection.CreateFile("HotPinkPong.sqlite");
+//            //sql = "create table WinsLosses (ID INTEGER PRIMARY KEY, Wins INTEGER, Losses INTEGER)";
+//            //DataBaseQueryExcecute(sql);
+
+//            //sql = "insert into WinsLosses (ID, Wins, Losses) values (1,0,0)";
+//            //DataBaseQueryExcecute(sql);
+
+//            sql = "select * from WinsLosses";
+//            DataBaseQueryRead(sql);
+//        }
+//        public void DataBaseQueryExcecute(string sql)
+//        {
+//            SQLiteConnection Conn = DBOpen();
+//            SQLiteCommand command = new SQLiteCommand(sql, Conn);
+//            command.ExecuteNonQuery();
+//            DBClose(Conn);
+//        }
+//        public void DataBaseQueryRead(string sql)
+//        {
+//            SQLiteConnection Conn = DBOpen();
+//            SQLiteCommand command = new SQLiteCommand(sql, Conn);
+//            SQLiteDataReader reader = command.ExecuteReader();
+//            while (reader.Read())
+//                System.Console.WriteLine("ID:" + reader.GetInt32(0) + " WIN: " + reader.GetInt32(1) + " LOSE: " + reader.GetInt32(2));
+//            DBClose(Conn);
+//        }
+//        public SQLiteConnection DBOpen()
+//        {
+//            SQLiteConnection m_dbConnection;
+//            m_dbConnection = new SQLiteConnection("Data Source=HotPinkPong.sqlite;Version=3");
+//            m_dbConnection.Open();
+//            return m_dbConnection;
+//        }
+//        public void DBClose(SQLiteConnection m_dbConnection)
+//        {
+//            m_dbConnection.Close();
+//        }
+//    }
+//}
---
+++ SFML/MonogameLogic/Project4/Visitor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Project4
+{
+    /// <summary>
+    /// Implementation of the visitor design pattern. 
+    /// Allows the code to safely visit certain parts of the code.
+    /// </summary>
+    public interface IOption<T>
+    {
+        void Visit(Action onNone, Action<T> onSome);
+        U Visit<U>(Func<U> onNone, Func<T, U> onSome);
+    }
+
+    public class None<T> : IOption<T>
+    {
+        public void Visit(Action onNone, Action<T> onSome)
+        {
+            onNone();
+        }
+
+        public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+        {
+            return onNone();
+        }
+    }
+    public class Some<T> : IOption<T>
+    {
+        T value;
+        public Some(T value) { this.value = value; }
+
+        public void Visit(Action onNone, Action<T> onSome)
+        {
+            onSome(value);
+        }
+
+        public U Visit<U>(Func<U> onNone, Func<T, U> onSome)
+        {
+            return onSome(value);
+        }
+    }
+}
